14767daac548bc49915a48265fbb356dfb7a2f36
Dovydas Berzanskas <dovydas.berzanskas@gmail.com>
2024-01-19 21:25:37 +0200

    [ESHOP] add banner to eshop, created welcome page, get products list implementation, get product list with condition example, create beans in config package to create product mock data using javafaker, use lombok Builder instead of constructor, use lombok log4j2 with configuration options, uuid added to product as data, product update by uuid using path variable for update, product delete by uuid using path variable for delete

--- src/main/java/lt/codeacademy/eshop/product/Product.java
+++ src/main/java/lt/codeacademy/eshop/product/Product.java
@@ -2,12 +2,23 @@ package lt.codeacademy.eshop.product;
 
 import lombok.*;
 
-@ToString
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Product {
+    private UUID productId;
     private String name;
     private double price;
     private int amount;
---
+++ src/main/java/lt/codeacademy/eshop/product/ProductController.java
@@ -0,0 +1,70 @@
+package lt.codeacademy.eshop.product;
+
+import lombok.extern.log4j.Log4j2;
+import lt.codeacademy.eshop.HttpEndpoints;
+import lt.codeacademy.eshop.product.Product;
+import lt.codeacademy.eshop.product.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@Log4j2
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping(HttpEndpoints.PRODUCTS_CREATE)
+    public String getFormForCreate(Model model) {
+        log.atInfo().log("-=== GET PRODUCT ON CREATE ===-");
+        model.addAttribute("product", Product.builder().build());
+
+        return "product/product";
+    }
+
+    @GetMapping(HttpEndpoints.PRODUCTS_UPDATE)
+    public String getFormForUpdate(Model model, @PathVariable UUID productId) {
+        log.atInfo().log("-=== GET PRODUCT ON UPDATE ===-");
+        model.addAttribute("product", productService.getProductByUUID(productId));
+
+        return "product/product";
+    }
+
+    @PostMapping("/products/create")
+        public String createAProduct(Model model, Product product) {
+        productService.saveProduct(product);
+
+        return getProducts(model);
+        }
+
+    @PostMapping(HttpEndpoints.PRODUCTS_UPDATE)
+    public String updateProduct(Model model, Product product, @PathVariable UUID productId) {
+        productService.updateProduct(product);
+
+        return getProducts(model);
+    }
+
+    @GetMapping(HttpEndpoints.PRODUCTS)
+    public String getProducts(Model model) {
+        final List<Product> allProducts = productService.getAllProducts();
+        model.addAttribute("productList", allProducts);
+
+        return "product/products";
+    }
+
+    @GetMapping(HttpEndpoints.PRODUCTS_DELETE)
+    public String deleteProduct (Model model, @PathVariable UUID productId) {
+        productService.deleteProductByUUID(productId);
+
+        return getProducts(model);
+    }
+}
