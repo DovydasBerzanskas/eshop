f1eb618732cb6b6b72a564959644aaec806535e8
Dovydas Berzanskas <dovydas.berzanskas@gmail.com>
2024-01-21 15:19:53 +0200

    [ESHOP] Further working with db data from dao layer

--- src/main/java/lt/codeacademy/eshop/product/ProductJDBCDao.java
+++ src/main/java/lt/codeacademy/eshop/product/ProductJDBCDao.java
@@ -1,5 +1,7 @@
 package lt.codeacademy.eshop.product;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collections;
@@ -7,24 +9,50 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
+@RequiredArgsConstructor
 public class ProductJDBCDao implements ProductDao{
 
-    @Override
-    public void save (Product product) {}
+    private final JdbcTemplate jdbcTemplate;
 
     @Override
-    public void update (Product product) {}
+    public void save (Product product) {
+        product.setProductId(UUID.randomUUID());
+        jdbcTemplate.update("INSERT INTO PRODUCT(PRODUCT_ID, NAME, PRICE, AMOUNT) VALUES (?, ?, ?, ?)",
+                product.getProductId(), product.getName(), product.getPrice(), product.getAmount());
+    }
+
+    @Override
+    public void update (Product product) {
+        jdbcTemplate.update("UPDATE PRODUCT SET name = ?, price = ?, amount = ? WHERE product_id = ?",
+                product.getName(), product.getPrice(), product.getAmount(), product.getProductId());
+    }
 
     @Override
     public List<Product> getAll() {
-        return Collections.emptyList();
+        return jdbcTemplate.query(
+                "SELECT * FROM PRODUCT", (rs, rowNum) -> Product.builder()
+                        .productId(UUID.fromString(rs.getString("product_id")))
+                        .name(rs.getString("name"))
+                        .price(rs.getDouble("price"))
+                        .amount(rs.getInt("amount"))
+                        .build());
     }
 
     @Override
     public Product getProductByUUID(UUID id) {
-        return null;
+        final List<Product> products = jdbcTemplate.query(
+            String.format("SELECT * FROM PRODUCT WHERE product_id = '%s'", id.toString()),
+                (rs, rowNum) -> Product.builder()
+                        .productId(UUID.fromString(rs.getString("product_id")))
+                        .name(rs.getString("name"))
+                        .price(rs.getDouble("price"))
+                        .amount(rs.getInt("amount"))
+                        .build());
+        return products.get(0);
     }
 
     @Override
-    public void deleteProductByUUID(UUID id) {}
+    public void deleteProductByUUID(UUID id) {
+        jdbcTemplate.execute(String.format("DELETE FROM PRODUCT WHERE PRODUCT_ID = '%s'", id.toString()));
+    }
 }
