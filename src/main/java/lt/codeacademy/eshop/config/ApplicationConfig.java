38ab96d827e8b20e8090d89fbee0e632e9a9c28c
Dovydas Berzanskas <dovydas.berzanskas@gmail.com>
2024-02-03 11:48:42 +0200

    [ESHOP] fixed missing @Configuration annotation in ApplicationConfig, fixed bugs in product.html, added WelcomeController

--- src/main/java/lt/codeacademy/eshop/config/ApplicationConfig.java
+++ src/main/java/lt/codeacademy/eshop/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package lt.codeacademy.eshop.config;
 import lt.codeacademy.eshop.helper.MessageService;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
+@Configuration
 public class ApplicationConfig implements WebMvcConfigurer {
 
     @Bean
