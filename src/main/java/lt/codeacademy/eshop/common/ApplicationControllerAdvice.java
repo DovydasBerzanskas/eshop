32c1743d3f161e761c625fa69aa68f265b67ac99
Dovydas Berzanskas <dovydas.berzanskas@gmail.com>
2024-02-03 14:33:11 +0200

    [ESHOP] add custom 4xx error page, add ExceptionHandler to handle error with attributes and return template

---
+++ src/main/java/lt/codeacademy/eshop/common/ApplicationControllerAdvice.java
@@ -0,0 +1,14 @@
+package lt.codeacademy.eshop.common;
+
+import lt.codeacademy.eshop.product.exception.ProductNotFoundException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler
+    public String productNotFound(ProductNotFoundException e, Model model) {
+        model.addAttribute("productUUID", e.getProductUUID());
+        return "product/error/productNotFound";
+    }
+}
--- src/main/java/lt/codeacademy/eshop/product/controllers/ProductController.java
+++ src/main/java/lt/codeacademy/eshop/product/controllers/ProductController.java
@@ -7,6 +7,7 @@ import lt.codeacademy.eshop.HttpEndpoints;
 import lt.codeacademy.eshop.helper.MessageService;
 import lt.codeacademy.eshop.product.Product;
 import lt.codeacademy.eshop.product.dto.ProductDto;
+import lt.codeacademy.eshop.product.exception.ProductNotFoundException;
 import lt.codeacademy.eshop.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
