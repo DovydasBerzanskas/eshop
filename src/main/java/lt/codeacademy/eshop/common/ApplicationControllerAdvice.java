6f24d5d0572cf6972aff03e157d16049862ff36a
Dovydas Berzanskas <dovydas.berzanskas@gmail.com>
2024-02-03 16:13:43 +0200

    [ESHOP] add global ModelAttribute

--- src/main/java/lt/codeacademy/eshop/common/ApplicationControllerAdvice.java
+++ src/main/java/lt/codeacademy/eshop/common/ApplicationControllerAdvice.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class ApplicationControllerAdvice {
@@ -21,5 +24,10 @@ public class ApplicationControllerAdvice {
     public void initBinder(WebDataBinder webDataBinder) {
         StringTrimmerEditor trimmerEditor = new StringTrimmerEditor(true);
         webDataBinder.registerCustomEditor(String.class, trimmerEditor);
+    }
+
+    @ModelAttribute("modelDateAttr")
+    public Date now() {
+        return new Date();
     }
 }
