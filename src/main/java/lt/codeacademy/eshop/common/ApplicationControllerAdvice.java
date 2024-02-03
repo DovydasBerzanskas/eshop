11043333d814cfe6b9580156c1a8c2ea71b65dd2
Dovydas Berzanskas <dovydas.berzanskas@gmail.com>
2024-02-03 16:09:44 +0200

    [ESHOP] InitBinder example to trim spaces for string text

--- src/main/java/lt/codeacademy/eshop/common/ApplicationControllerAdvice.java
+++ src/main/java/lt/codeacademy/eshop/common/ApplicationControllerAdvice.java
@@ -1,14 +1,25 @@
 package lt.codeacademy.eshop.common;
 
 import lt.codeacademy.eshop.product.exception.ProductNotFoundException;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 
+@ControllerAdvice
 public class ApplicationControllerAdvice {
 
     @ExceptionHandler
     public String productNotFound(ProductNotFoundException e, Model model) {
         model.addAttribute("productUUID", e.getProductUUID());
         return "product/error/productNotFound";
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        StringTrimmerEditor trimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, trimmerEditor);
     }
 }
