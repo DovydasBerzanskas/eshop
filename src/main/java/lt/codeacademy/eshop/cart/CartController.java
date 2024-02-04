5c45c38da297c94702eee5adef5a282a35614e76
Dovydas Berzanskas <dovydas.berzanskas@gmail.com>
2024-02-04 11:05:31 +0200

    [ESHOP] create session and retrieve session data

--- src/main/java/lt/codeacademy/eshop/cart/CartController.java
+++ src/main/java/lt/codeacademy/eshop/cart/CartController.java
@@ -1,15 +1,26 @@
 package lt.codeacademy.eshop.cart;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @RequestMapping("/cart")
+@SessionAttributes("cartSession")
 public class CartController {
 
     @GetMapping
     public String openCart() {
         return "/cart/cart";
+    }
+
+    @GetMapping("/add")
+    public String addToCart(Model model) {
+        model.addAttribute("cartSession", "Basic message");
+
+        return "redirect:/cart";
     }
 }
