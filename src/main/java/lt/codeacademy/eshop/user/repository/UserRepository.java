50c19c76e3ce663266cf3123dee8cd9cb79e5cd1
Dovydas Berzanskas <dovydas.berzanskas@gmail.com>
2024-02-11 15:10:17 +0200

    [ESHOP] fix authentication (login) error using LAZY fetch as using EAGER is a bad practice

--- src/main/java/lt/codeacademy/eshop/user/repository/UserRepository.java
+++ src/main/java/lt/codeacademy/eshop/user/repository/UserRepository.java
@@ -2,10 +2,15 @@ package lt.codeacademy.eshop.user.repository;
 
 import lt.codeacademy.eshop.user.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findUserByEmail(String email);
+    @Query(value =
+    "SELECT u FROM User u" +
+    "JOIN FETCH u.authorities" +
+    "WHERE u.email = :email")
+    Optional<User> findUserByEmailWithAuthorities(String email);
 }
