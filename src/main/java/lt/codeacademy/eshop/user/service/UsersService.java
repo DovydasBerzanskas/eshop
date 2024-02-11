b06cb9c623c57cd3a2cd0eebf4b3e8d4cae9e65f
Dovydas Berzanskas <dovydas.berzanskas@gmail.com>
2024-02-11 14:35:28 +0200

    [ESHOP] have full working user registration form with PasswordEncoder bean, separated into different services

--- src/main/java/lt/codeacademy/eshop/user/service/UsersService.java
+++ src/main/java/lt/codeacademy/eshop/user/service/UsersService.java
@@ -1,13 +1,10 @@
 package lt.codeacademy.eshop.user.service;
 
 import lombok.RequiredArgsConstructor;
-import lt.codeacademy.eshop.user.dto.UserDto;
-import lt.codeacademy.eshop.user.pojo.User;
 import lt.codeacademy.eshop.user.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,16 +19,4 @@ public class UsersService implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("'" + username + "' not found!"));
     }
 
-    public void register(UserDto userDto) {
-        userRepository.save(
-                User.builder()
-                        .name(userDto.getName())
-                        .surname(userDto.getSurname())
-                        .email(userDto.getEmail())
-                        .phoneNumber(userDto.getPhoneNumber())
-                        .zipCode(userDto.getZipCode())
-                        .password(new BCryptPasswordEncoder().encode(userDto.getPassword()))
-                        .build()
-        );
-    }
 }
