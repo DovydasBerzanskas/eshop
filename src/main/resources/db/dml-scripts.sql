7250c45dc83193a28814f042fd5f16bb7b3873d3
Dovydas Berzanskas <dovydas.berzanskas@gmail.com>
2024-02-11 16:02:16 +0200

    [ESHOP] add constraints for security dll tables to prevent data duplication and improve registration persistence

--- src/main/resources/db/dml-scripts.sql
+++ src/main/resources/db/dml-scripts.sql
@@ -24,8 +24,8 @@ VALUES (1, 2),
        (2, 1);
 
 INSERT INTO users(name, surname, email, password, zip_code, phone_number)
-    VALUES ('Admin', 'Admin', 'admin@eshop.lt', '$2a$10$RaoyLY5ZVehcXsqk65jKcexHb/LLXkvjQfILcF/oo8lPJBjeO001m', 'LT-0001', 'N/A');
-            ('User', 'User', 'user@eshop.lt', '$2a$10$RaoyLY5ZVehcXsqk65jKcexHb/LLXkvjQfILcF/oo8lPJBjeO001m', 'LT-0002', 'N/A');
+    VALUES ('Admin', 'Admin', 'admin@eshop.lt', '$2a$10$RaoyLY5ZVehcXsqk65jKcexHb/LLXkvjQfILcF/oo8lPJBjeO001m', 'LT-0001', '+37061234567');
+            ('User', 'User', 'user@eshop.lt', '$2a$10$RaoyLY5ZVehcXsqk65jKcexHb/LLXkvjQfILcF/oo8lPJBjeO001m', 'LT-0002', '+37061234568');
 
 INSERT INTO authority(name, description)
 VALUES ('ADMIN', NULL),
