feb6f8a7b8673868354a78d12e2b3a6fdc3a1b03
Dovydas Berzanskas <dovydas.berzanskas@gmail.com>
2024-02-18 12:25:43 +0200

    [ESHOP] update ProductDto to have multiple categories as per the relationship

--- src/main/java/lt/codeacademy/eshop/product/mappers/ProductMapper.java
+++ src/main/java/lt/codeacademy/eshop/product/mappers/ProductMapper.java
@@ -4,9 +4,11 @@ import lt.codeacademy.eshop.common.mapper.Mapper;
 import lt.codeacademy.eshop.product.dto.ProductCategoryDto;
 import lt.codeacademy.eshop.product.dto.ProductDto;
 import lt.codeacademy.eshop.product.pojo.Product;
+import lt.codeacademy.eshop.product.pojo.ProductCategory;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
 @Component
 public class ProductMapper implements Mapper<Product, ProductDto> {
@@ -16,6 +18,7 @@ public class ProductMapper implements Mapper<Product, ProductDto> {
                 .productId(product.getProductId())
                 .name(product.getName())
                 .price(product.getPrice())
+                .categoryId(product.getProductCategories().stream().map(ProductCategory::getId).collect(Collectors.toList()))
                 .amount(product.getAmount())
                 .build();
     }
